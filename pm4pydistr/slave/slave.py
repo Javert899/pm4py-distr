c2ddcbd5ce8f4d6732328a48a1bd30b70943ff2e
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-07-10 14:42:30 +0200

    Automatic log allocation

--- pm4pydistr/slave/slave.py
+++ pm4pydistr/slave/slave.py
@@ -1,11 +1,13 @@
 from pm4pydistr.configuration import PARAMETERS_PORT, PARAMETERS_HOST, PARAMETERS_MASTER_HOST, PARAMETERS_MASTER_PORT, \
-    PARAMETERS_CONF
+    PARAMETERS_CONF, BASE_FOLDER_LIST_OPTIONS
 
 from pm4pydistr.slave.slave_service import SlaveSocketListener
 from pm4pydistr.slave.slave_requests import SlaveRequests
+from pathlib import Path
+from pm4py.objects.log.importer.parquet import factory as parquet_importer
 
 import os
-
+import shutil
 
 class Slave:
     def __init__(self, parameters):
@@ -28,7 +30,19 @@ class Slave:
         self.slave_requests.register_to_webservice()
 
     def create_folder(self, folder_name):
-        print("create folder " + str(folder_name))
+        #print("create folder " + str(folder_name))
+        if not os.path.isdir(os.path.join(self.conf, folder_name)):
+            os.mkdir(os.path.join(self.conf, folder_name))
 
     def load_log(self, folder_name, log_name):
-        print("loading log " + str(log_name)+" into "+str(folder_name))
+        #print("loading log " + str(log_name)+" into "+str(folder_name))
+        if not os.path.exists(os.path.join(self.conf, folder_name, log_name)):
+            for folder in BASE_FOLDER_LIST_OPTIONS:
+                if folder_name in os.listdir(folder):
+                    list_paths = parquet_importer.get_list_parquet(os.path.join(folder, folder_name))
+                    list_paths_corr = {}
+                    for x in list_paths:
+                        list_paths_corr[Path(x).name] = x
+                    if log_name in list_paths_corr:
+                        #print("log_name",log_name," in ",os.path.join(folder, folder_name),list_paths_corr[log_name])
+                        shutil.copyfile(list_paths_corr[log_name], os.path.join(self.conf, folder_name, log_name))
