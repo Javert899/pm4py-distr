3c2eaa4d37b5ead820b20e42bd6a41468c169e3a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-07-28 14:23:45 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,40 @@
+from os.path import dirname, join
+
+from setuptools import setup
+
+import pm4pydistr
+
+
+def read_file(filename):
+    with open(join(dirname(__file__), filename)) as f:
+        return f.read()
+
+
+setup(
+    name=pm4pydistr.__name__,
+    version=pm4pydistr.__version__,
+    description=pm4pydistr.__doc__.strip(),
+    long_description=read_file('README.md'),
+    author=pm4pydistr.__author__,
+    author_email=pm4pydistr.__author_email__,
+    py_modules=[pm4pydistr.__name__],
+    include_package_data=True,
+    packages=['pm4pydistr', 'pm4pydistr.slave', 'pm4pydistr.master', 'pm4pydistr.master.rqsts',
+              'pm4pydistr.log_handlers'],
+    url='http://www.pm4py.org',
+    license='GPL 3.0',
+    install_requires=[
+        'pm4py',
+        'networkx>=2.2',
+        'bpmn_python==0.0.18',
+        'intervaltree',
+        'lime',
+        'joblib',
+        'pydotplus'
+    ],
+    project_urls={
+        'Documentation': 'http://pm4py.pads.rwth-aachen.de/documentation/',
+        'Source': 'https://github.com/pm4py/pm4py-distr',
+        'Tracker': 'https://github.com/pm4py/pm4py-distr/issues',
+    }
+)
