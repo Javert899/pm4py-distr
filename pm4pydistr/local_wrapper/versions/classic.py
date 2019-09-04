ce49fbc215e00fb6cf68fc123732a5fd8b7028fe
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-09-04 12:33:16 +0200

    DFG calculation in local wrapper

--- pm4pydistr/local_wrapper/versions/classic.py
+++ pm4pydistr/local_wrapper/versions/classic.py
@@ -1,18 +1,21 @@
 from pm4pydistr.local_wrapper.distr_log_obj import DistrLogObj
 from pm4pydistr.log_handlers import parquet as parquet_handler
-from pm4py.objects.log.importer.parquet.versions import pyarrow
+from pm4py.objects.log.importer.parquet import factory as parquet_factory
+from pathlib import Path
 
 class ClassicDistrLogObject(DistrLogObj):
     def __init__(self, distr_log_path, parameters=None):
         DistrLogObj.__init__(self, distr_log_path, parameters=parameters)
 
     def get_list_logs(self):
-        return pyarrow.apply(self.distr_log_path)
+        lp = parquet_factory.get_list_parquet(self.distr_log_path)
+
+        return [Path(log).name for log in lp]
 
     def calculate_dfg(self, parameters=None):
         list_logs = self.get_list_logs()
-        return parquet_handler.calculate_dfg(".", self.distr_log_path, list_logs, parameters=parameters)
-
+        dfg = parquet_handler.calculate_dfg(".", self.distr_log_path, list_logs, parameters=parameters)
+        return {(x.split("@@")[0], x.split("@@")[1]): dfg[x] for x in dfg}
 
 def apply(path, parameters=None):
     if parameters is None:
