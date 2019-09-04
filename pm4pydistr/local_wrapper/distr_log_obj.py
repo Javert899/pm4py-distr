2f0c9a2df0467329bba0f975303e983deff336c4
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-09-04 12:27:56 +0200

    Intermediate commit

--- pm4pydistr/local_wrapper/distr_log_obj.py
+++ pm4pydistr/local_wrapper/distr_log_obj.py
@@ -1,3 +1,9 @@
 class DistrLogObj(object):
     def __init__(self, distr_log_path, parameters=None):
+        if parameters is None:
+            parameters = {}
         self.distr_log_path = distr_log_path
+        self.init_parameters = parameters
+
+    def calculate_dfg(self, parameters=None):
+        raise Exception("not implemented")
--- pm4pydistr/local_wrapper/versions/classic.py
+++ pm4pydistr/local_wrapper/versions/classic.py
@@ -1,9 +1,17 @@
 from pm4pydistr.local_wrapper.distr_log_obj import DistrLogObj
-
+from pm4pydistr.log_handlers import parquet as parquet_handler
+from pm4py.objects.log.importer.parquet.versions import pyarrow
 
 class ClassicDistrLogObject(DistrLogObj):
     def __init__(self, distr_log_path, parameters=None):
         DistrLogObj.__init__(self, distr_log_path, parameters=parameters)
+
+    def get_list_logs(self):
+        return pyarrow.apply(self.distr_log_path)
+
+    def calculate_dfg(self, parameters=None):
+        list_logs = self.get_list_logs()
+        return parquet_handler.calculate_dfg(".", self.distr_log_path, list_logs, parameters=parameters)
 
 
 def apply(path, parameters=None):
