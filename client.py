39ff4e9bee5458a3a14f44d93580ad9b8b37c2b8
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-09-11 16:36:08 +0200

    Small change

--- client.py
+++ client.py
@@ -1,16 +1,19 @@
 import requests
 import time
-
+import json
+import numpy as np
 
 host = "137.226.117.71"
 port = "5001"
 service = "calculateDfg"
 keyphrase = "hello"
-process = "roadtraffic"
+process = "bigdistr0"
 
 url = "http://"+host+":"+port+"/"+service+"?keyphrase="+keyphrase+"&process="+process
 aa = time.time()
 r = requests.get(url)
 bb = time.time()
 print(r.text)
-print("time required to calculate the DFG ",(bb-aa))
+#dfg = json.loads(r.text)["dfg"]
+#print(np.sum(list(dfg.values())))
+print("time required to call the service ",(bb-aa))
