39fa3a59ee0cb548d73f78135d6c28b99c1a1583
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-10-02 09:53:33 +0200

    Turning on slaves more prudently

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ class ExecutionThread(Thread):
 
 t1 = ExecutionThread(PYTHON_PATH+" launch.py --type master --conf master --port 5001")
 t1.start()
-time.sleep(0.5)
 t2 = ExecutionThread(PYTHON_PATH+" launch.py --type slave --conf slave1 --port 5002 --master-host 127.0.0.1 --master-port 5001")
 t2.start()
 t3 = ExecutionThread(PYTHON_PATH+" launch.py --type slave --conf slave2 --port 5003 --master-host 127.0.0.1 --master-port 5001")
