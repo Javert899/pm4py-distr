4da287c8e52854a5ff908f85ac7da9b734863c15
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-10-14 10:56:12 +0200

    CPU allocation

--- set_task_priority.py
+++ set_task_priority.py
@@ -1,6 +1,12 @@
 import psutil
+import os
 
+
+cpu_count = 0
 for proc in psutil.process_iter():
     print(dir(proc))
     print(proc)
-
+    if proc.name.startswith("python3 launch.py "):
+        os.system("taskset -cp "+str(cpu_count)+" "+str(proc.pid))
+        cpu_count = cpu_count + 1
+print("allocated cpu=",cpu_count)
