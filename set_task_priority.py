dd8f1f51a6c97c1c18f3296cdec514fdecd18c67
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-10-14 11:05:10 +0200

    Added re-nicing

--- set_task_priority.py
+++ set_task_priority.py
@@ -7,5 +7,6 @@ for proc in psutil.process_iter():
     name = " ".join(proc.cmdline())
     if name.startswith("python3 launch.py "):
         os.system("taskset -cp "+str(cpu_count)+" "+str(proc.pid))
+        os.system("renice -20 -p "+str(proc.pid))
         cpu_count = cpu_count + 1
 print("allocated cpu=",cpu_count)
