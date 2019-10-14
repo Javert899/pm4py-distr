c6fd78035c174c94068e675c16012764cd8dd30f
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-10-14 11:01:50 +0200

    Fix

--- set_task_priority.py
+++ set_task_priority.py
@@ -4,8 +4,8 @@ import os
 
 cpu_count = 0
 for proc in psutil.process_iter():
-    print(proc.name())
-    if proc.name().startswith("python3 launch.py "):
+    name = " ".join(proc.cmdline())
+    if name.startswith("python3 launch.py "):
         os.system("taskset -cp "+str(cpu_count)+" "+str(proc.pid()))
         cpu_count = cpu_count + 1
 print("allocated cpu=",cpu_count)
