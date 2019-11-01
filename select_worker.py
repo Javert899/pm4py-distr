b58504ff9a7c9cdb1242bcc42cff705c339925b0
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-11-01 09:57:34 +0100

    Update scripts

--- select_worker.py
+++ select_worker.py
@@ -4,21 +4,21 @@ import os
 ip_address = str(socket.gethostbyname(socket.gethostname()))
 
 if ip_address == "137.226.117.71":
-    os.system("python3 server_launch.py")
+    os.system("nohup python3 server_launch.py &")
     time.sleep(15)
-    os.system("python3 worker6.py")
+    os.system("nohup python3 worker6.py &")
 elif ip_address == "137.226.117.72":
     time.sleep(20)
-    os.system("python3 worker5.py")
+    os.system("nohup python3 worker5.py &")
 elif ip_address == "137.226.117.73":
     time.sleep(20)
-    os.system("python3 worker1.py")
+    os.system("nohup python3 worker1.py &")
 elif ip_address == "137.226.117.74":
     time.sleep(20)
-    os.system("python3 worker2.py")
+    os.system("nohup python3 worker2.py &")
 elif ip_address == "137.226.117.75":
     time.sleep(20)
-    os.system("python3 worker3.py")
+    os.system("nohup python3 worker3.py &")
 elif ip_address == "137.226.117.76":
     time.sleep(20)
-    os.system("python3 worker4.py")
+    os.system("nohup python3 worker4.py &")
