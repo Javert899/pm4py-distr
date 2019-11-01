7ff637e363527c6190ce235b7a886553110c0d4b
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-11-01 09:56:43 +0100

    Update scripts

---
+++ select_worker.py
@@ -0,0 +1,24 @@
+import socket
+import time
+import os
+ip_address = str(socket.gethostbyname(socket.gethostname()))
+
+if ip_address == "137.226.117.71":
+    os.system("python3 server_launch.py")
+    time.sleep(15)
+    os.system("python3 worker6.py")
+elif ip_address == "137.226.117.72":
+    time.sleep(20)
+    os.system("python3 worker5.py")
+elif ip_address == "137.226.117.73":
+    time.sleep(20)
+    os.system("python3 worker1.py")
+elif ip_address == "137.226.117.74":
+    time.sleep(20)
+    os.system("python3 worker2.py")
+elif ip_address == "137.226.117.75":
+    time.sleep(20)
+    os.system("python3 worker3.py")
+elif ip_address == "137.226.117.76":
+    time.sleep(20)
+    os.system("python3 worker4.py")
---
+++ update.sh
@@ -0,0 +1,11 @@
+pkill -f launch
+git pull
+pip3 install -U -r requirements.txt
+pip3 install -U pm4pycvxopt
+pip3 uninstall -y pm4py
+cd ..
+cd pm4py-source
+pip3 install -U -r requirements.txt
+python3 setup.py install
+cd ..
+cd pm4py-distr
