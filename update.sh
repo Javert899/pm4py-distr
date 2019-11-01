cf6bb41b39abba2b790cf328e55ffbebe13a1df9
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-11-01 10:13:04 +0100

    Fix

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@ pkill -f launch
 git clean -x -f
 git reset --hard HEAD
 rm -rRf slave*
+rm -f nohup.out
 git pull
 pip3 install -U -r requirements.txt
 pip3 install -U pm4pycvxopt
@@ -10,11 +11,11 @@ cd ..
 cd pm4py-source
 git clean -x -f
 git reset --hard HEAD
-rm -rRf build dist
+rm -rRf build dist pm4py.egg-info
 git pull
 pip3 install -U -r requirements.txt
 python3 setup.py install
-rm -rRf build dist
+rm -rRf build dist pm4py.egg-info
 cd ..
 cd pm4py-distr
 nohup python3 select_worker.py &
