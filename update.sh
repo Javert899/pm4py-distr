83b4bb5e375f823e74fdcee319c0d1a2068bfddd
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-11-01 10:05:45 +0100

    Fix

--- update.sh
+++ update.sh
@@ -1,10 +1,13 @@
 pkill -f launch
+git reset --hard HEAD
 git pull
 pip3 install -U -r requirements.txt
 pip3 install -U pm4pycvxopt
 pip3 uninstall -y pm4py
 cd ..
 cd pm4py-source
+git clean -x -f
+git reset --hard HEAD
 git pull
 pip3 install -U -r requirements.txt
 python3 setup.py install
