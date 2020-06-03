7c52fc139123bcf07eca14812940c010c69a4cbe
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-06-03 07:32:30 +0200

    update

--- update.sh
+++ update.sh
@@ -5,18 +5,4 @@ rm -rRf slave*
 rm -f nohup.out
 git pull
 pip3 install -U -r requirements.txt
-pip3 install -U pm4pycvxopt
-pip3 install --no-deps pm4pyigraph
-pip3 uninstall -y pm4py
-cd ..
-cd pm4py-source
-git clean -x -f
-git reset --hard HEAD
-rm -rRf build dist pm4py.egg-info
-git pull
-pip3 install -U -r requirements.txt
-python3 setup.py install
-rm -rRf build dist pm4py.egg-info
-cd ..
-cd pm4py-distr
 nohup python3 select_worker.py &
