f542777e0dde1d377f90e62c2ef617e7eec2de0a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-06-03 09:31:07 +0200

    added new examples and modified existing

---
+++ examples/receipt_remote_conformance.py
@@ -0,0 +1,26 @@
+import inspect
+import os
+import sys
+import unittest
+import time
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+from pm4pydistr.remote_wrapper import factory as wrapper_factory
+from pm4py.algo.discovery.inductive import factory as inductive_miner
+
+# possibility to limit the number of sublogs (per slave) that are considered
+max_no_samples = 5
+# create the wrapper
+wrapper = wrapper_factory.apply("212.237.8.106", "5001", "hello", "receipt", parameters={"no_samples": max_no_samples})
+# gets the log summary (number of cases and number of events)
+print(wrapper.get_log_summary())
+dfg = wrapper.calculate_dfg()
+net, im, fm = inductive_miner.apply_dfg(dfg)
+tbr_result = wrapper.perform_tbr_net_variants(net, im, fm, var_list=None)
+aa = time.time()
+aligned_traces = wrapper.perform_alignments_net_variants(net, im, fm, var_list=None)
+bb = time.time()
+print(aligned_traces)
+print(bb-aa)
