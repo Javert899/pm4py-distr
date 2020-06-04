14956dba85f553e788e90cd12dbe6a3be7ffd892
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-06-04 08:02:04 +0200

    fix ip

--- examples/receipt_remote_conformance.py
+++ examples/receipt_remote_conformance.py
@@ -13,7 +13,7 @@ from pm4py.algo.discovery.inductive import factory as inductive_miner
 # possibility to limit the number of sublogs (per slave) that are considered
 max_no_samples = 5
 # create the wrapper
-wrapper = wrapper_factory.apply("212.237.8.106", "5001", "hello", "receipt", parameters={"no_samples": max_no_samples})
+wrapper = wrapper_factory.apply("137.226.117.71", "5001", "hello", "receipt", parameters={"no_samples": max_no_samples})
 # gets the log summary (number of cases and number of events)
 print(wrapper.get_log_summary())
 dfg = wrapper.calculate_dfg()
