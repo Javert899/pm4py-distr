a5338fc89f9cfa11f744c9a6d0bd77ab18c1150b
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-06-16 11:37:37 +0200

    new

--- spark-start.sh
+++ spark-start.sh
@@ -3,7 +3,7 @@ cd /opt/spark/sbin
 
 if [ "$HOSTNAME" = "padsname1" ]; then
     ./start-master.sh
-    sleep 1 && ./start-slave.sh spark://137.226.117.71:7077
+    sleep 5 && ./start-slave.sh spark://137.226.117.71:7077
 else
     sleep 5 && ./start-slave.sh spark://137.226.117.71:7077
 fi
