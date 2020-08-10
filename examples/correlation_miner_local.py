12bfbce1aa5f4df803000cc4a5717db28f3c53a5
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-08-10 11:04:54 +0200

    removed file

--- examples/correlation_miner_local.py
+++
@@ -1,6 +0,0 @@
-from pm4pydistr.local_wrapper import factory as wrapper_factory
-
-wrapper = wrapper_factory.apply("../master/receipt")
-dfg, performance_dfg = wrapper.correlation_miner()
-print(dfg)
-print(performance_dfg)
