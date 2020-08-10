c24c4774bb4fdf26489eb50aa9915a0a40e42e3f
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-08-10 10:39:50 +0200

    fixes

--- pm4pydistr/master/rqsts/corr_mining_req.py
+++ pm4pydistr/master/rqsts/corr_mining_req.py
@@ -5,11 +5,11 @@ import json
 
 
 class CorrMiningRequest(BasicMasterRequest):
-    def __init__(self, session, target_host, target_port, use_transition, no_samples, process, content):
+    def __init__(self, session, target_host, target_port, use_transition, no_samples, process, json_content):
         self.session = session
         self.target_host = target_host
         self.target_port = target_port
-        self.content = content
+        self.json_content = json_content
         self.use_transition = use_transition
         self.no_samples = no_samples
         self.process = process
@@ -23,7 +23,8 @@ class CorrMiningRequest(BasicMasterRequest):
         uri = "http://" + self.target_host + ":" + self.target_port + "/correlationMiner?keyphrase=" + KEYPHRASE + "&process=" + str(
             self.process) + "&session=" + str(self.session) + "&use_transition=" + str(
             self.use_transition) + "&no_samples=" + str(self.no_samples)
-        r = requests.post(uri)
-        resp = json.loads(r)
+        print(self.json_content)
+        r = requests.post(uri, json=self.json_content)
+        resp = json.loads(r.text)
         self.PS_matrix = resp["PS_matrix"]
         self.duration_matrix = resp["duration_matrix"]
