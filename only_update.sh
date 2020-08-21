92f6d1d783b1c8ebbfdf94b89be65e20b485af18
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-08-21 13:36:22 +0200

    only update script

---
+++ only_update.sh
@@ -0,0 +1,10 @@
+pkill -f custom_launch
+git clean -x -f
+git reset --hard HEAD
+rm -rRf slave*
+rm -f nohup.out
+git pull
+pip3 install -U pm4pydistr
+pip3 install -U -r requirements.txt
+pip3 uninstall pm4py
+pip3 install -U pm4pyexperimental
