1a53f8615522563f257ae8cab6e7d44a2be1ee57
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-08-31 11:00:28 +0200

    commit

--- update.sh
+++ update.sh
@@ -1,14 +1,2 @@
-pkill -f custom_launch
-git clean -x -f
-git reset --hard HEAD
-rm -rRf slave*
-rm -f nohup.out
-git pull
-pip3 uninstall ortools
-pip3 install -U pm4pydistr
-pip3 install -U -r requirements.txt
-pip3 uninstall pm4py
-pip3 install -U pm4pyexperimental
+bash only_update.sh
 nohup python3 select_worker.py &
-pip3 install -U cvxopt
-pip3 install --no-deps pm4pycvxopt==0.0.9
