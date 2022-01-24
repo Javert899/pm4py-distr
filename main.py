94127b55caf652953fe7837ac3866fcab5879a70
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-01-24 14:07:29 +0100

    automatic CPU count in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from pm4pydistr.configuration import PYTHON_PATH
 from threading import Thread
+import multiprocessing
 
 import time
 
@@ -14,7 +15,9 @@ class ExecutionThread(Thread):
 
 t1 = ExecutionThread(PYTHON_PATH+" launch.py type master conf master port 5001")
 t1.start()
-t2 = ExecutionThread(PYTHON_PATH+" launch.py type slave conf slave1 autoport 1 masterhost 127.0.0.1 masterport 5001")
-t2.start()
-t3 = ExecutionThread(PYTHON_PATH+" launch.py type slave conf slave2 autoport 1 masterhost 127.0.0.1 masterport 5001")
-t3.start()
+
+N = multiprocessing.cpu_count()
+
+for i in range(N):
+	t = ExecutionThread(PYTHON_PATH+" launch.py type slave conf slave"+str(i)+" autoport 1 masterhost 127.0.0.1 masterport 5001")
+	t.start()
